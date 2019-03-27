10ebf5d5d01181883375d8b46672d1a35b0d77ce
JOSHUA KILE <JOSKILE@uat.edu>
2019-03-26 17:59:24 -0700

    added the opening story and RNG weapon need to work on examin body, wepon used, and room with body

--- 08 Assignment  Interactive Fiction/Source.cpp
+++ 08 Assignment  Interactive Fiction/Source.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <ctime>
+#include <algorithm>
 //Create an Interactive Fiction game.
 //Create a C++ program that uses more than one function.
 //Control all your functions through the main function.
@@ -12,6 +14,8 @@ using namespace std;
 // Must declare functions before use!
 string GetCharacterName();
 void welcomeUser(string CharacterName);
+void openingStory(string CharacterName);
+
 
 
 int main()
@@ -24,10 +28,10 @@ int main()
 	// here we use char name
 	welcomeUser(CharacterName);
 
-	cout << "You are standing in apartment number 123, Which just so happens to be the scene\n";
-	cout << "of a terrable murder, your the ace detective " << CharacterName << "and it is your job to \n";
-	cout << "find the cause of death, murder weapon "
-	
+	openingStory(CharacterName);
+
+
+
 	system("pause");
 
 	return 0;
@@ -47,19 +51,24 @@ string GetCharacterName()
 void welcomeUser(string CharacterName)
 {
 	cout << "\nWelcome " << CharacterName << " to Slaughterhouse.\n";
-	cout << "You must find a murder weapon and solve the case.\n";
+	cout << "You must find a murder weapon and solve the case.\n\n\n";
 }
 
-string getMurderWeapon()
+void openingStory(string CharacterName)
 {
-	enum fields { WORD, HINT, NUM_FIELDS };
-	const int NUM_WORDS = 5;
-	const string WORDS[NUM_WORDS][NUM_FIELDS] =
-	{
-		{"wall", "Do you feel you're banging your head against something?"},
-		{"glasses", "These might help you see the answer."},
-		{"labored", "Going slowly, is it?"},
-		{"persistent", "Keep at it."},
-		{"jumble", "come on man, it's just the name of the game."}
-	}
+	cout << "You are standing in apartment number 123, Which just so happens to be the scene\n";
+	cout << "of a terrable murder. your the ace detective " << CharacterName << " and it is your job to \n";
+	cout << "find the cause of death, murder weapon, and suspect.";
 }
+
+//string getMurderWeapon()
+//{
+	//// Create a collection of 10 words you had wrote down manually
+	//vector<string> weapons;  //collection of possible words to guess
+	//weapons.push_back("GUN");
+	//weapons.push_back("KNIFE");
+	//weapons.push_back("BAT");
+
+	//srand(static_cast<unsigned int>(time(0)));
+	//random_shuffle(weapons.begin(), weapons.end());
+//}
